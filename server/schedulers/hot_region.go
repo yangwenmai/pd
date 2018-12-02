2edf03819454f1ecb3ce5d4fb1c14eea833927f4
yangwenmai <yangwen.yw@gmail.com>
2018-12-02 10:43:17 +0800

    feat(server/schedulers/hot_region): use ml scheduler and change model name

--- server/schedulers/hot_region.go
+++ server/schedulers/hot_region.go
@@ -387,7 +387,7 @@ func postJSON(s string, ms []Feature, srcStoreID, destStoreID uint64, srcRegion 
 	return nil
 }
 
-var reqURL = "http://106.75.11.4:8000/model/xxx1"
+var reqURL = "http://localhost:8000/model/pd"
 
 func httpClient(method, jsonStr string, srcStoreID, destStoreID uint64) uint64 {
 	logStr := "[HT]method:" + method + ", URL:>" + reqURL
